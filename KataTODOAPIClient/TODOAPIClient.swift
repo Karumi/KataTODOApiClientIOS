be6634629530afca94fe6344e7063e65a18eb3f1
Pedro Vicente Gomez <pedro@gokarumi.com>
2016-02-12 10:08:33 +0100

    Add main skeleton to the api client

---
+++ KataTODOAPIClient/TODOAPIClient.swift
@@ -0,0 +1,20 @@
+//
+//  TODOAPIClient.swift
+//  KataTODOAPIClient
+//
+//  Created by Pedro Vicente Gomez on 12/02/16.
+//  Copyright © 2016 Karumi. All rights reserved.
+//
+
+import Foundation
+import BothamNetworking
+
+public class TODOAPIClient {
+
+    private let botham: BothamAPIClient
+
+    init() {
+        self.botham = BothamAPIClient(baseEndpoint: TODOAPIClientConfig.baseEndpoint)
+    }
+
+}
---
+++ KataTODOAPIClient/TODOAPIClientConfig.swift
@@ -0,0 +1,16 @@
+//
+//  TODOAPIClientConfig.swift
+//  KataTODOAPIClient
+//
+//  Created by Pedro Vicente Gomez on 12/02/16.
+//  Copyright © 2016 Karumi. All rights reserved.
+//
+
+import Foundation
+
+struct TODOAPIClientConfig {
+
+    static let baseEndpoint = "http://jsonplaceholder.typicode.com"
+    static let tasksEndpoint = "/todos"
+
+}
