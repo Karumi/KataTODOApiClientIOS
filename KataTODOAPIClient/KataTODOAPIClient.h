f94e1cde8e4ec0a649da30d5b3b88e8b0eb68f09
Pedro Vicente Gomez <pedro@gokarumi.com>
2016-02-12 09:51:27 +0100

    Empty project with some extra configuration files and basic LICENSE and CONTRIBUTING information

---
+++ KataTODOAPIClient/KataTODOAPIClient.h
@@ -0,0 +1,19 @@
+//
+//  KataTODOAPIClient.h
+//  KataTODOAPIClient
+//
+//  Created by Pedro Vicente Gomez on 12/02/16.
+//  Copyright © 2016 Karumi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KataTODOAPIClient.
+FOUNDATION_EXPORT double KataTODOAPIClientVersionNumber;
+
+//! Project version string for KataTODOAPIClient.
+FOUNDATION_EXPORT const unsigned char KataTODOAPIClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KataTODOAPIClient/PublicHeader.h>
+
+
