c5cd48eaacd5b660eeafde611ad5155f48d6dd14
Sergio Gutierrez <sergio@gokarumi.com>
2017-07-27 12:51:15 +0200

    Fix the build in travis and other versions of Swift
    Fix dependency versions and update cocoapods version
    Use cocoapods version 1.2.1
    Update the new version of cocoapods
    Add extension for Nocilla methods to prevent issues with comparison of JSON serialized objects

---
+++ KataTODOAPIClientTests/LSStubRequestDSL+Json.swift
@@ -0,0 +1,48 @@
+//
+//  LSStubResponseDSL+Json.swift
+//  KataTODOAPIClient
+//
+//  Created by Sergio Gutiérrez on 28/07/2017.
+//  Copyright © 2017 Karumi. All rights reserved.
+//
+
+import Foundation
+import Nocilla
+import SwiftyJSON
+
+extension LSStubRequestDSL {
+    open func withJsonBody(_ jsonString: NSString) -> LSStubRequestDSL? {
+        let normalizedJsonString = JSON(data: jsonString.data()).rawString()!
+        return self.withBody(JsonMatcheable(jsonString: normalizedJsonString))
+    }
+}
+
+private class JsonMatcheable: NSObject, LSMatcheable {
+    private let jsonString: String
+
+    public init(jsonString: String) {
+        self.jsonString = jsonString
+    }
+
+    func matcher() -> LSMatcher! {
+        return JsonMatcher(jsonString: jsonString)
+    }
+}
+
+private class JsonMatcher: LSMatcher {
+
+    private let jsonString: String
+
+    public init(jsonString: String) {
+        self.jsonString = jsonString
+    }
+
+    override func matches(_ string: String!) -> Bool {
+        return matchesData(string.data(using: String.Encoding.utf8)!)
+    }
+
+    override func matchesData(_ data: Data!) -> Bool {
+        let json = JSON(data: data)
+        return jsonString == json.rawString()!
+    }
+}
---
+++ KataTODOAPIClientTests/LSStubResponseDSL+Json.swift
@@ -0,0 +1,18 @@
+//
+//  LSStubResponseDSL+Json.swift
+//  KataTODOAPIClient
+//
+//  Created by Sergio Gutiérrez on 28/07/2017.
+//  Copyright © 2017 Karumi. All rights reserved.
+//
+
+import Foundation
+import Nocilla
+import SwiftyJSON
+
+extension LSStubResponseDSL {
+    open func withJsonBody(_ jsonString: NSString) -> LSStubResponseDSL? {
+        let normalizedJsonString = JSON(data: jsonString.data()).rawString()!
+        return self.withBody(NSString(string: normalizedJsonString))
+    }
+}
