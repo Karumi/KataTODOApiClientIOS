37bdd7d75f089a22cc91dfd1c4984913f07b7f6d
Antonio Candinho <antonio@gokarumi.com>
2021-09-06 10:26:17 +0200

    Update playground with latest changes

--- TODOAPIClientPlayground.playground/Contents.swift
+++ TODOAPIClientPlayground.playground/Contents.swift
@@ -1,37 +1,41 @@
 //: TODOAPIClient Playground
 
-import PlaygroundSupport
+import Combine
 import KataTODOAPIClient
+import PlaygroundSupport
 
 PlaygroundPage.current.needsIndefiniteExecution = true
 
 //: Create a TODOAPIClient instance.
 let apiClient = TODOAPIClient()
-
+var subscriptions = Set<AnyCancellable>()
 //: Get all the task already created.
-apiClient.getAllTasks { result in
-    print(result)
-}
+apiClient.getAllTasks().sink(
+    receiveCompletion: { _ in },
+    receiveValue: { print("Got all tasks: \($0.count) tasks") }
+).store(in: &subscriptions)
 
 //: Get a task using the task id.
-apiClient.getTaskById(1) { result in
-    print(result)
-}
+apiClient.getTaskById(1).sink(
+    receiveCompletion: { _ in },
+    receiveValue: { print("Got task by id: \($0)") }
+).store(in: &subscriptions)
 
 //: Add a new task to a user.
-apiClient.addTaskToUser(1, title: "Finish this kata", completed: false) { result in
-    print(result)
-}
+apiClient.addTaskToUser(1, title: "Finish this kata", completed: false).sink(
+    receiveCompletion: { _ in },
+    receiveValue: { print("Added task to user: \($0)") }
+).store(in: &subscriptions)
 
 //: Delete a task using the task id.
-apiClient.deleteTaskById(1) { result in
-    print(result)
-}
+apiClient.deleteTaskById(1).sink(
+    receiveCompletion: { _ in },
+    receiveValue: { print("Deleted task: id 1") }
+).store(in: &subscriptions)
 
 //: Update a taks.
 let taskToUpdate = TaskDTO(userId: 1, id: 2, title: "Finish this kata", completed: false)
-apiClient.updateTask(taskToUpdate) { result in
-    if case let .success(updatedTask) = result {
-        print(updatedTask)
-    }
-}
+apiClient.updateTask(taskToUpdate).sink(
+    receiveCompletion: { _ in },
+    receiveValue: { print("Updated task: \($0)") }
+).store(in: &subscriptions)
